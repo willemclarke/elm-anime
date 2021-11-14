5df1cb60369e0b06953a9bea372d93be8989f542
willemclarke <willem.2@hotmail.com>
2021-11-14 21:01:12 +1000

    feat(query): got a working nested query, hoorah

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,12 @@
 module Main exposing (..)
 
+-- import AniList.Enum.MediaType.MediaType as MediaType
+
+import AniList.Enum.MediaFormat
+import AniList.Enum.MediaType
 import AniList.Object
 import AniList.Object.Media as Media
+import AniList.Object.MediaTitle as MediaTitle
 import AniList.Object.Page as Page
 import AniList.Query as Query
 import Browser
@@ -20,10 +25,14 @@ import RemoteData exposing (RemoteData)
 
 
 
--- query {
---   Page(page: 1) {
---     media {
+-- {
+--   Page(page: 1, perPage: 20) {
+--     media(type: MANGA) {
 --       id
+--       averageScore
+--       title {
+--         english
+--       }
 --     }
 --   }
 -- }
@@ -31,23 +40,43 @@ import RemoteData exposing (RemoteData)
 
 
 type alias Response =
-    Maybe Media
+    Maybe Page
+
+
+type alias Page =
+    { media : Maybe (List (Maybe Media)) }
 
 
 type alias Media =
-    { id : Int, averageScore : Maybe Int }
+    { id : Int, averageScore : Maybe Int, title : Maybe Title }
 
 
-query : SelectionSet (Maybe Media) RootQuery
+type alias Title =
+    { english : Maybe String }
+
+
+query : SelectionSet (Maybe Page) RootQuery
 query =
-    Query.media (\optionals -> { optionals | id = Present 1 }) mediaSelection
+    Query.page (\optionals -> { optionals | page = Present 1, perPage = Present 10 }) pageSelection
+
+
+pageSelection : SelectionSet Page AniList.Object.Page
+pageSelection =
+    SelectionSet.map Page (Page.media identity mediaSelection)
 
 
 mediaSelection : SelectionSet Media AniList.Object.Media
 mediaSelection =
-    SelectionSet.map2 Media
+    SelectionSet.map3 Media
         Media.id
         Media.averageScore
+        (Media.title titleSelection)
+
+
+titleSelection : SelectionSet Title AniList.Object.MediaTitle
+titleSelection =
+    SelectionSet.map Title
+        (MediaTitle.english identity)
 
 
 makeRequest : Cmd Msg
